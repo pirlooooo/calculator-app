3a68c529e2efd9b0fa1b1861b268f046171b6774
pirlooooo <pirlo9125@gmail.com>
2026-04-08 15:00:23 +0000

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
+int a = 10;
+int b = 6;
 
+static int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+static int pengurangan(int a, int b)
+{
+    return a - b;
+}
+static int perkalian(int a, int b)
+{
+    return a * b;
+}
+static int pembagian(int a, int b)
+{
+    return a / b;
+}
+
+Console.WriteLine("penjumlahan: " + a + " + " + b + " = " + penjumlahan(a, b));
+Console.WriteLine("pengurangan: " + a + " - " + b + " = " + pengurangan(a, b));
+Console.WriteLine("perkalian: " + a + " * " + b + " = " + perkalian(a, b));
+Console.WriteLine("pembagian: " + a + " / " + b + " = " + pembagian(a, b));
